468ff732e7ee4ee083b529df0c33e9af36685ecc
borysiam <borysiam@gmail.com>
2014-04-16 17:36:54 +0200

    Fix authentication bug.

--- app/mturk/main/management/commands/crawler/auth.py
+++ app/mturk/main/management/commands/crawler/auth.py
@@ -1,27 +1,38 @@
+import cookielib
+import re
 import urllib
 import urllib2
-import cookielib
 
 from django.conf import settings
 
-AMAZON_AUTH_URL = 'https://www.amazon.com/gp/aws/ssop/index.html?awscbctx=&awscbid=urn%3Aaws%3Asid%3A027Y0TCSPRG5XHFYJSR2&awscredential=&awsnoclientpipeline=true&awsstrict=false&awsturknosubway=true&wa=wsignin1.0&wctx=&wreply={}%3A443%2Fmturk%2Fendsignin&wtrealm=urn%3Aaws%3Asid%3A027Y0TCSPRG5XHFYJSR2&awssig=yZwe4P9pATXcyDaYEFCDKycNYIM%3D'.format(
-    urllib.quote_plus(settings.MTURK_PAGE)
-)
+
+AUTH_PAGE = '{}/mturk'.format(settings.MTURK_PAGE)
 
 
 def install_opener():
-    """Install global opener with cookies support
-    """
+    """Install global opener with cookies support."""
     jar = cookielib.CookieJar()
     opener = urllib2.build_opener(urllib2.HTTPCookieProcessor(jar))
+    # Amazon displays 'Please enable cookies (...)' without fake 'User-agent'
+    # header.
+    opener.addheaders = [('User-agent', 'Mozilla/5.0')]
     urllib2.install_opener(opener)
     return opener
 
 
 def authenticate(email, password):
     """Authenticate using mturk worker account. This should be done after
-    installation of opener with cookie support.
-    """
-    data = {'action': 'sign-in', 'password': password, 'email': email}
-    resp = urllib2.urlopen(AMAZON_AUTH_URL, urllib.urlencode(data))
-    return resp
+    installation of opener with cookie support."""
+    # It should redirect to form with hidden fields containing all necessary
+    # tokens to authenticate.
+    response = urllib2.urlopen('{}/beginsignin'.format(AUTH_PAGE))
+    html = response.read()
+    # Parse auth form fields.
+    data = re.findall(r'input.+?name="(.+?)".+?value="(.+?)"', html)
+    query = dict(data)
+    query.update({
+        'email': email,
+        'password': password,
+        'create': 0,  # Force password authentication.
+    })
+    return urllib2.urlopen(response.url, urllib.urlencode(query))
